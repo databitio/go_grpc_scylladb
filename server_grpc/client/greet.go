971eb7636d57f7936a101feab413948425c97495
databitio <mccain1330@gmail.com>
2022-06-05 11:10:05 -0500

    edited unary and stream functions for server and client; added ssl script for future https communication

--- server_grpc/client/greet.go
+++ server_grpc/client/greet.go
@@ -10,12 +10,12 @@ import (
 func doGreet(c pb.GreetServiceClient) {
 	log.Println("doGreet was invoked")
 	res, err := c.Greet(context.Background(), &pb.GreetRequest{
-		FirstName: "Johnny",
+		FirstName: "paul",
 	})
 
 	if err != nil {
 		log.Fatalf("Could not greet: %v\n", err)
 	}
 
-	log.Printf("Greeting %s\n", res.Result)
+	log.Printf("Greeting %s\n", res)
 }
