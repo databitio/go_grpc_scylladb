9ca5f83b2a3c3c5d6c82284ddce861615acfaac2
databitio <mccain1330@gmail.com>
2022-06-08 17:24:39 -0500

    done

--- queries/queries.go
+++ queries/queries.go
@@ -1,22 +1,14 @@
 package queries
 
 import (
-	// "encoding/json"
 	"fmt"
 	"log"
-	"time"
 
-	// "errors"
-	// "net/http"
-
-	// "github.com/gin-gonic/gin"
 	"github.com/databitio/go_server/datatypes"
 
-	"github.com/bxcodec/faker/v3"
 	"github.com/gocql/gocql"
 	"github.com/scylladb/gocqlx/v2"
 	"github.com/scylladb/gocqlx/v2/qb"
-	"github.com/scylladb/gocqlx/v2/table"
 )
 
 //perhaps future cqlx session object
@@ -25,41 +17,32 @@ type Gosess struct {
 }
 
 //*************************************************************************
-//Metadata and create table used to filter data
-func CreateTicketMetadata() table.Metadata {
-	ticketMetadata := table.Metadata{
-		Name:    "meed.ticket",
-		Columns: []string{"ticketid", "title", "userid", "description", "reward", "lifespan", "type", "archived"},
-		PartKey: []string{"ticketid"},
-		SortKey: []string{"userid"},
+func CreateTicket(session gocqlx.Session, newTicket *datatypes.Ticket) error {
+
+	insertTicket := qb.Insert("meed.ticket").
+		Columns("ticketid",
+			"userid",
+			"serverid",
+			"title",
+			"description",
+			"reward",
+			"lifespan",
+			"type",
+			"archived",
+			"status",
+			"claimed").Query(session)
+
+	insertTicket.BindStruct(*newTicket)
+
+	if err := insertTicket.ExecRelease(); err != nil {
+		log.Fatal("ExecRelease() failed:", err)
+		return err
 	}
-	return ticketMetadata
-}
-
-func CreateTable(metadata table.Metadata) *table.Table {
-	return table.New(metadata)
-}
-
-func DBToTicket(ticket map[string]interface{}) (datatypes.Ticket, error) {
-
-	newTicket := &datatypes.Ticket{
-		Ticketid:    ticket["ticketid"].(gocql.UUID),
-		Userid:      ticket["userid"].(gocql.UUID),
-		Serverid:    ticket["serverid"].(gocql.UUID),
-		Title:       ticket["title"].(string),
-		Description: ticket["description"].(string),
-		Reward:      ticket["reward"].(string),
-		Lifespan:    ticket["lifespan"].(time.Time),
-		Type:        ticket["type"].(string),
-		Archived:    ticket["archived"].(bool),
-		Status:      ticket["status"].(string),
-		Claimed:     ticket["claimed"].(bool),
-	}
-
-	return *newTicket, nil
+	return nil
 }
 
 //*************************************************************************
+//Get ticket by id
 func GetByID(session gocqlx.Session, uuid gocql.UUID) (datatypes.Ticket, error) {
 	w := qb.EqNamed("ticketid", "")
 	q := qb.Select("meed.ticket").Where(w).Query(session).Bind(uuid.String())
@@ -71,7 +54,28 @@ func GetByID(session gocqlx.Session, uuid gocql.UUID) (datatypes.Ticket, error) 
 	ticket, err := DBToTicket(rows[0])
 	return ticket, err
 }
+//*************************************************************************
+//Get all tickets in the ticket table
+func GetAllTickets(session gocqlx.Session) ([]datatypes.Ticket, error) {
 
+	q := qb.Select("meed.ticket").Query(session)
+
+	defer q.Release()
+
+	var tickets []datatypes.Ticket
+	if rows, err := q.Iter().SliceMap(); err == nil {
+		for _, row := range rows {
+			ticket, err := DBToTicket(row)
+			if err == nil {
+				tickets = append(tickets, ticket)
+			}
+		}
+	}
+	return tickets, nil
+}
+
+//*************************************************************************
+//Update a ticket by ticketid, userid, and serverid
 func UpdateTicket(session gocqlx.Session, ticket *datatypes.Ticket) error {
 	// w := qb.EqNamed("ticketid", "")
 	// x := qb.EqNamed("serverid", "")
@@ -113,28 +117,25 @@ func UpdateTicket(session gocqlx.Session, ticket *datatypes.Ticket) error {
 	fmt.Println("Updated successfully!")
 	return nil
 }
+//*************************************************************************
+//Delete a ticket by ticketid
+func DeleteTicket(session gocqlx.Session, uuid gocql.UUID) error {
 
-func GetAllTickets(session gocqlx.Session) ([]datatypes.Ticket, error) {
+	w := qb.EqNamed("ticketid", uuid.String())
+	deleteTicket := qb.Delete("meed.ticket").Where(w).Query(session).Bind(uuid.String())
 
-	q := qb.Select("meed.ticket").Query(session)
-
-	defer q.Release()
-
-	var tickets []datatypes.Ticket
-	if rows, err := q.Iter().SliceMap(); err == nil {
-		for _, row := range rows {
-			ticket, err := DBToTicket(row)
-			if err == nil {
-				tickets = append(tickets, ticket)
-			}
-		}
+	if err := deleteTicket.ExecRelease(); err != nil {
+		log.Fatal("ExecRelease() failed:", err)
+		return err
 	}
-	return tickets, nil
+
+	fmt.Println("deleted successfully!")
+	return nil
 }
+//*************************************************************************
+//Create new ticket table
+func CreateTicketTable(session gocqlx.Session) error {
 
-func CreateNewServer(session gocqlx.Session) error {
-
-	session.ExecStmt(`DROP KEYSPACE meed`)
 	err := session.ExecStmt(`CREATE KEYSPACE IF NOT EXISTS meed WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 2}`)
 	if err != nil {
 		fmt.Println("create keyspace:", err)
@@ -162,85 +163,26 @@ func CreateNewServer(session gocqlx.Session) error {
 
 	return nil
 }
+//*************************************************************************
+//Deletes ticket table
+// func DeleteTicketTable (session gocqlx.Session) error {
+// 	err := session.ExecStmt(`DROP KEYSPACE meed`)
+// 	if err != nil {
+// 		log.Printf("Error deleting ticket table: %v\n", err)
+// 	}
+// 	return nil
+// }
+//*************************************************************************
+// func SelectTicketByLocalID(session gocqlx.Session, ticketTable *table.Table, uuid gocql.UUID) {
+// 	ticketrows := ticketTable.SelectQuery(session)
+// 	ticketrows.BindStruct(&datatypes.Ticket{
+// 		Ticketid: uuid,
+// 	})
+// 	var tickets []datatypes.Ticket
 
-func SelectTicketByLocalID(session gocqlx.Session, ticketTable *table.Table, uuid gocql.UUID) {
-	ticketrows := ticketTable.SelectQuery(session)
-	ticketrows.BindStruct(&datatypes.Ticket{
-		Ticketid: uuid,
-	})
-	var tickets []datatypes.Ticket
-
-	if err := ticketrows.Select(&tickets); err != nil {
-		log.Fatal("Select() failed:", err)
-	}
-	fmt.Println(tickets)
-	fmt.Println("selected correctly!")
-}
-
-func CreateFakeTicket() datatypes.Ticket {
-
-	newTicket := datatypes.Ticket{}
-	err := faker.FakeData(&newTicket)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return newTicket
-}
-
-func CreateTicket(session gocqlx.Session, newTicket *datatypes.Ticket) error {
-
-	insertTicket := qb.Insert("meed.ticket").
-		Columns("ticketid",
-			"userid",
-			"serverid",
-			"title",
-			"description",
-			"reward",
-			"lifespan",
-			"type",
-			"archived",
-			"status",
-			"claimed").Query(session)
-
-	insertTicket.BindStruct(*newTicket)
-
-	if err := insertTicket.ExecRelease(); err != nil {
-		log.Fatal("ExecRelease() failed:", err)
-		return err
-	}
-	return nil
-}
-
-// insertTicket.BindStruct(ticket{
-// 	Ticketid: uuid,
-// 	Title: "this is the insert query",
-// 	Userid: "johnny",
-// 	Description: "automatic insert",
-// 	Reward: "the joy of not having to rewrite this",
-// 	Lifespan: 5,
-// 	Type: "service",
-// 	Archived: false,
-// })
-
-func MustParseUUID(s string) gocql.UUID {
-	u, err := gocql.ParseUUID(s)
-	if err != nil {
-		panic(err)
-	}
-	return u
-}
-
-//Couldn't get Delete cqlx to work; improvised with Delete statement
-func DeleteTicket(session gocqlx.Session, uuid gocql.UUID) error {
-
-	w := qb.EqNamed("ticketid", uuid.String())
-	deleteTicket := qb.Delete("meed.ticket").Where(w).Query(session).Bind(uuid.String())
-
-	if err := deleteTicket.ExecRelease(); err != nil {
-		log.Fatal("ExecRelease() failed:", err)
-		return err
-	}
-
-	fmt.Println("deleted successfully!")
-	return nil
-}
+// 	if err := ticketrows.Select(&tickets); err != nil {
+// 		log.Fatal("Select() failed:", err)
+// 	}
+// 	fmt.Println(tickets)
+// 	fmt.Println("selected correctly!")
+// }
--- server_grpc/client/main.go
+++ server_grpc/client/main.go
@@ -3,10 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
-	"time"
 
-	"github.com/databitio/go_server/datatypes"
-	"github.com/databitio/go_server/queries"
 	pb "github.com/databitio/go_server/server_grpc/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -23,7 +20,15 @@ func main() {
 	defer conn.Close()
 	c := pb.NewTicketServiceClient(conn)
 
-	// ticket := &pb.TicketInfo{
+	fmt.Println(c)
+
+}
+
+//Testing all CRUD operations
+	// uuid := queries.MustParseUUID("44573233-4c12-1d06-2c63-0910604a1816")
+	// time, _ := time.Parse("2006-01-02T15:04:05.000Z", "2232-12-23 00:00:00 +0000 UTC")
+
+		// ticket := &pb.TicketInfo{
 	// 	Ticketid:    "44573233-4c12-1d06-2c63-0910604a1816",
 	// 	Userid:      "44573233-4c12-1d06-2c63-0910604a1816",
 	// 	Serverid:    "44573233-4c12-1d06-2c63-0910604a1816",
@@ -37,59 +42,54 @@ func main() {
 	// 	Claimed:     false,
 	// }
 	// fmt.Println("ticket made!")
-	uuid := queries.MustParseUUID("44573233-4c12-1d06-2c63-0910604a1816")
-	time, _ := time.Parse("2006-01-02T15:04:05.000Z", "2232-12-23 00:00:00 +0000 UTC")
 
-	newTicket := datatypes.Ticket{
-		Ticketid:    uuid,
-		Serverid:    uuid,
-		Userid:      uuid,
-		Title:       "this is created ticket query",
-		Description: "newly created ticket",
-		Reward:      "newly created ticket",
-		Lifespan:    time,
-		Type:        "service",
-		Archived:    false,
-		Status:      "updated!",
-		Claimed:     false,
-	}
+	// newTicket := datatypes.Ticket{
+	// 	Ticketid:    uuid,
+	// 	Serverid:    uuid,
+	// 	Userid:      uuid,
+	// 	Title:       "this is created ticket query",
+	// 	Description: "newly created ticket",
+	// 	Reward:      "newly created ticket",
+	// 	Lifespan:    time,
+	// 	Type:        "service",
+	// 	Archived:    false,
+	// 	Status:      "updated!",
+	// 	Claimed:     false,
+	// }
 
-	ticketinfo := ticketToTicketMessage(&newTicket)
+	// ticketinfo := ticketToTicketMessage(&newTicket)
 
-	fmt.Println("Reading all tickets...")
-	alltickets := readTickets(c)
-	for index, ticket := range alltickets {
-		fmt.Println(index, *ticketMessageToTicket(ticket))
-	}
-	fmt.Println("Getting ticket by id...")
-	myticket := goGetTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
-	fmt.Printf("Ticket found!: %v\n", myticket)
-	newTicket.Title = "this is the updated title! hooray!"
-	fmt.Println("Updating this tickets title...")
-	ticketinfo = ticketToTicketMessage(&newTicket)
-	_, err = goUpdateTicket(c, ticketinfo)
-	if err != nil {
-		fmt.Printf("Error updating ticket: %v\n", err)
-	}
-	fmt.Println("Ticket updated successfully!")
-	myticket = goGetTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
-	fmt.Printf("New ticket: %v\n", myticket)
-	fmt.Println("Deleting this ticket from db...")
-	err = goDeleteTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
-	if err != nil {
-		fmt.Printf("Error deleting ticket: %v\n", err)
-	}
-	fmt.Println("Delete success! Printing out all tickets...")
-	alltickets = readTickets(c)
-	for index, ticket := range alltickets {
-		fmt.Println(index, *ticketMessageToTicket(ticket))
-	}
-	fmt.Println("Adding ticket back to database...")
-	err = goCreateTicket(c, ticketinfo)
-	if err != nil {
-		fmt.Printf("Creating deleting ticket: %v\n", err)
-	}
-	fmt.Println("Successfully added ticket! Shutting down...")
-	// readTickets(c)
-	// fmt.Println(myticket)
-}
+	// fmt.Println("Reading all tickets...")
+	// alltickets := readTickets(c)
+	// for index, ticket := range alltickets {
+	// 	fmt.Println(index, *ticketMessageToTicket(ticket))
+	// }
+	// fmt.Println("Getting ticket by id...")
+	// myticket := goGetTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
+	// fmt.Printf("Ticket found!: %v\n", myticket)
+	// newTicket.Title = "this is the updated title! hooray!"
+	// fmt.Println("Updating this tickets title...")
+	// ticketinfo = ticketToTicketMessage(&newTicket)
+	// _, err = goUpdateTicket(c, ticketinfo)
+	// if err != nil {
+	// 	fmt.Printf("Error updating ticket: %v\n", err)
+	// }
+	// fmt.Println("Ticket updated successfully!")
+	// myticket = goGetTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
+	// fmt.Printf("New ticket: %v\n", myticket)
+	// fmt.Println("Deleting this ticket from db...")
+	// err = goDeleteTicket(c, "44573233-4c12-1d06-2c63-0910604a1816")
+	// if err != nil {
+	// 	fmt.Printf("Error deleting ticket: %v\n", err)
+	// }
+	// fmt.Println("Delete success! Printing out all tickets...")
+	// alltickets = readTickets(c)
+	// for index, ticket := range alltickets {
+	// 	fmt.Println(index, *ticketMessageToTicket(ticket))
+	// }
+	// fmt.Println("Adding ticket back to database...")
+	// err = goCreateTicket(c, ticketinfo)
+	// if err != nil {
+	// 	fmt.Printf("Creating deleting ticket: %v\n", err)
+	// }
+	// fmt.Println("Successfully added ticket! Shutting down...")
