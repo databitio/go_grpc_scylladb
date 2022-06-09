556dc83b3807f54b2786dae79f220511e9a7dfad
databitio <mccain1330@gmail.com>
2022-06-09 11:34:43 -0500

    beginning node implementation of client

---
+++ server_grpc/clientjs/main.js
@@ -0,0 +1,41 @@
+// const path = require("path")
+const grpc = require("@grpc/grpc-js");
+const protoLoader = require("@grpc/proto-loader");
+var PROTO_PATH = "../proto/server_grpc.proto";
+
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+
+var protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
+
+var routeguide = protoDescriptor.routeguide;
+var client = new routeguide.RouteGuide('localhost:50051', grpc.credentials.createInsecure());
+
+function main() {
+    client.goCreateTicket()
+}
+
+
+
+
+
+function getServer() {
+    var server = new grpc.Server();
+    server.addService(routeguide.RouteGuide.service, {
+      getFeature: getFeature,
+      listFeatures: listFeatures,
+      recordRoute: recordRoute,
+      routeChat: routeChat
+    });
+    return server;
+  }
+  var routeServer = getServer();
+  routeServer.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), () => {
+    routeServer.start();
+  });
